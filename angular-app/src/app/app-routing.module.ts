c85d88cf0239907c66cba3c92cf689a2f25a1e51
Simon <simon.brecl@gmail.com>
2018-02-22 18:54:31 +0100

    Initial commit

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // import { TransactionComponent } from './Transaction/Transaction.component'
 import { HomeComponent } from './home/home.component';
-
+/*
 import { IzdelekComponent } from './Izdelek/Izdelek.component';
 import { NarociloComponent } from './Narocilo/Narocilo.component';
 import { KupecComponent } from './Kupec/Kupec.component';
@@ -14,25 +14,26 @@ import { PosiljkaComponent } from './Posiljka/Posiljka.component';
 import { VseTransakcijeComponent } from './VseTransakcije/VseTransakcije.component';
 import { OddajaNarocilaTRComponent } from './OddajaNarocilaTR/OddajaNarocilaTR.component';
 import { PlaciloNaDomuTRComponent } from './PlaciloNaDomuTR/PlaciloNaDomuTR.component';
-
+*/
 
 
 const routes: Routes = [
     // { path: 'transaction', component: TransactionComponent },
     {path: '', component: HomeComponent},
-		
+		/*
 		{ path: 'Izdelek', component: IzdelekComponent},
 		
     { path: 'Narocilo', component: NarociloComponent},
     { path: 'Kupec', component: KupecComponent},
     { path: 'Prodajalec', component: ProdajalecComponent},
+    
     { path: 'PostnaEnota', component: PostnaEnotaComponent},
     { path: 'Postar', component: PostarComponent},
     { path: 'Banka', component: BankaComponent},
     { path: 'Posiljka', component: PosiljkaComponent},
     { path: 'VseTransakcije', component: VseTransakcijeComponent},
     { path: 'OddajaNarocilaTR', component: OddajaNarocilaTRComponent},
-    { path: 'PlaciloNaDomuTR', component: PlaciloNaDomuTRComponent},
+    { path: 'PlaciloNaDomuTR', component: PlaciloNaDomuTRComponent},*/
 
 		
 		{path: '**', redirectTo:''}
